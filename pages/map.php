cd60cc411aa21363cfc4d05e07b4c8b2110b2f60
Josh White <whitejosh429@gmail.com>
2026-04-15 23:20:26 +0100

    map url

--- pages/map.php
+++ pages/map.php
@@ -6,7 +6,7 @@
 
 <main-element welcome style="height:75vh;">
     <iframe style="border-radius:0.5em;border:none;"
-        src="https://zen-book-gore-correspondence.trycloudflare.com" 
+        src="https://opening-webpage-bolt-rhythm.trycloudflare.com/" 
         width="100%";
         height="100%";
         style="border: none;">
