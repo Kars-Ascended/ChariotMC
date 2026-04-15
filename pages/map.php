573a7d662e0fd40f62e9575a94c231c72830fde6
Josh White <whitejosh429@gmail.com>
2026-04-15 23:33:39 +0100

    map fix frfr

--- pages/map.php
+++ pages/map.php
@@ -6,7 +6,7 @@
 
 <main-element welcome style="height:75vh;">
     <iframe style="border-radius:0.5em;border:none;"
-        src="https://resolutions-partnerships-south-almost.trycloudflare.com/" 
+        src="https://wonderful-store-some-others.trycloudflare.com" 
         width="100%";
         height="100%";
         style="border: none;">
