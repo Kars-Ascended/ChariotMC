da8bb3b009fad5998d0fff42a13cb8f2e60bb8c6
Josh White <whitejosh429@gmail.com>
2026-04-24 21:30:00 +0100

    cloudflared fix

--- pages/map.php
+++ pages/map.php
@@ -6,7 +6,7 @@
 
 <main-element welcome style="height:75vh;">
     <iframe style="border-radius:0.5em;border:none;"
-        src="https://cam-commented-visit-reef.trycloudflare.com/" 
+        src="https://src-living-monster-actor.trycloudflare.com" 
         width="100%";
         height="100%";
         style="border: none;">
