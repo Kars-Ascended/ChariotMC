e6c5198aac5a6265806b574fa38e57eddc40387e
Josh White <whitejosh429@gmail.com>
2026-04-26 18:36:26 +0100

    proxy debug

--- pages/proxy.php
+++ pages/proxy.php
@@ -1,11 +1,14 @@
 <?php
-$url = 'http://node-de-01.tickhosting.com:40156' . ($_SERVER['REQUEST_URI'] ?? '/');
+$url = 'http://node-de-01.tickhosting.com:40156/';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 $response = curl_exec($ch);
-$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+$error = curl_error($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
-header('Content-Type: ' . $contentType);
-echo $response;
-?>
+
+echo "HTTP Code: $httpCode<br>";
+echo "Error: $error<br>";
+echo "Response length: " . strlen($response) . "<br>";
+echo "<pre>" . htmlspecialchars(substr($response, 0, 500)) . "</pre>";
