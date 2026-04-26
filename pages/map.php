1e68aaeca80f1abf1072d7a9f8f55ee2d7e664b8
Josh White <whitejosh429@gmail.com>
2026-04-26 18:42:46 +0100

    fuck iframes

--- pages/map.php
+++ pages/map.php
@@ -6,7 +6,7 @@
 
 <main-element welcome style="height:75vh;">
     <iframe style="border-radius:0.5em;border:none;"
-        src="proxy" 
+        src="https://lesser-comics-starts-patch.trycloudflare.com" 
         width="100%";
         height="100%";
         style="border: none;">
